66109d4000acbf57aec66447ebb663b4ab2f6125
Uiop Pilon <Circutrider21@outlook.com>
2020-01-11 12:08:36 -0600

    made a command line app

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-exports.version = function () {
-  // body...
-  var version = "1.0.2";
-  console.log(version);
-};
+#!/usr/bin/env node
 
-exports.parcf = function (filename) {
-  // body...
-  fs.readFile(filename, 'utf-8', (err, data) => {
-    if (err) throw err;
+const chalk = require("chalk");
+const boxen = require("boxen");
+const yargs = require("yargs");
 
-    // Converting Raw Buffer to text
-    // data using tostring function.
-    var info = JSON.parse(data)
+const options = yargs
+ .usage("Usage: -f <filename>")
+ .option("f", { alias: "filename", describe: "The name of the file to parse", type: "string", demandOption: true })
+ .argv;
 
-    state = info.state
-    country = info.country
-  }
-};
+ var filename = options.filename;
+
+ fs.readFile(filename, 'utf-8', (err, data) => {
+   if (err) throw err;
+
+   // Converting Raw Buffer to text
+   // data using tostring function.
+   var info = JSON.parse(data)
+   var creator = info.author
+   var version = info.version
+   var name = info.name
+   var description = info.description
+   var license = info.license
+ }
+
+console.log(chalk.green("${name} v${version}"));
+console.log(chalk.red("Under the ${license} license"));
+console.log(chalk.green("Created by ${creator}"));
+console.log(chalk.red("Description: ${description}");
