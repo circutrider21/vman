a9f864c08807a8fd4a66a114c6ebe380ea37c7cd
circutrider21 <57774961+circutrider21@users.noreply.github.com>
2020-02-26 17:38:06 +0000

    It actually works now

--- index.js
+++ index.js
@@ -3,28 +3,58 @@
 const chalk = require("chalk");
 const boxen = require("boxen");
 const yargs = require("yargs");
+const fs = require("fs");
 
-const options = yargs
- .usage("Usage: -f <filename>")
- .option("f", { alias: "filename", describe: "The name of the file to parse", type: "string", demandOption: true })
- .argv;
+/*function parse_arg_one(data) {
+  let filename = "";
+  switch(data) {
+  case "help":
+    // code block
+    break;
+  case "version":
+    // code block
+    Console.log("1.0.0");
+    break;
+  default:
+    // code block
+    filename = data;
+    return filename;
+  }
+}*/
 
- var filename = options.filename;
+var argv = require('yargs')
+  .options({
+    'file': {
+      alias: 'f',
+      describe: 'the path to the package.json',
+      demandOption: true
+    },
+    'about': {
+      alias: 'a',
+      describe: 'About this application',
+      demandOption: false
+    }
+  })
+  .help()
+  .argv
 
- fs.readFile(filename, 'utf-8', (err, data) => {
-   if (err) throw err;
+var filename = argv.file;
 
-   // Converting Raw Buffer to text
-   // data using tostring function.
-   var info = JSON.parse(data)
-   var creator = info.author
-   var version = info.version
-   var name = info.name
-   var description = info.description
-   var license = info.license
- }
 
-console.log(chalk.green("${name} v${version}"));
-console.log(chalk.red("Under the ${license} license"));
-console.log(chalk.green("Created by ${creator}"));
-console.log(chalk.red("Description: ${description}");
+var info = JSON.parse(fs.readFileSync(filename))
+var creator = info.author
+var version = info.version
+var name = info.name
+var description = info.description
+var license = info.license
+
+
+var lineone = name + " v" + version;
+var linetwo = "Under the " + license + " License"
+var linethree = "Created by " + creator;
+var linefour = "Description: " + description;
+
+console.log(chalk.green(lineone));
+console.log(chalk.red(linetwo));
+console.log(chalk.green(linethree));
+console.log(chalk.blue(linefour));
